48b4bf065d6969ce711c40316e6da6b44d8ce548
skeshavaa <s.keshavaa@gmail.com>
2021-01-24 18:44:05 -0500

    first commit, to project, firebase auth fully functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import fire from '../src/fire'
+import React, {useState, useEffect} from 'react'
+import Login from './login'
+import Hero from './hero.js'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [user, setUser] = useState("")
+  const [account, setAccount] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  }
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  }
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => {{
+        switch(err.code){
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message)
+            break;
+        }
+      }})
+  }
+
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch(err.code){
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  }
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  }
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user){
+        clearInputs();
+        setUser(user);
+      } else{
+        setUser("");
+      }
+    })
+  }
+  
+  useEffect(() => {
+    authListener();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { user ? (
+        <Hero handleLogout={handleLogout}/>
+      ) : 
+      (
+        <>
+        <Login email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleLogin={handleLogin} handleSignUp={handleSignUp} account={account} setAccount={setAccount} emailError={emailError} passwordError={passwordError}>
+        </Login>
+        </>
+      )}
+      
+      
     </div>
   );
 }
---
+++ src/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCsUrsDhZtqEsXVTzgPzB-RfZvXCgDrRPY",
+    authDomain: "shopsmall-17825.firebaseapp.com",
+    projectId: "shopsmall-17825",
+    storageBucket: "shopsmall-17825.appspot.com",
+    messagingSenderId: "788565713479",
+    appId: "1:788565713479:web:6eb70fb6f31019e69a8762",
+    measurementId: "G-8TWN8M9D80"
+  };
+  // Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
