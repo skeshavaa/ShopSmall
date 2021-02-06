62b3d2a46b9e0672513adf78fbb013adedafab30
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-02-05 21:28:24 -0500

    Added a hardcoded data for sample

---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+      "firstName": "Steven",
+      "lastName": "Limantoro",
+      "type": "Consumer"
+    },
+    {
+      "firstName": "John",
+      "lastName": "Doe",
+      "type": "Business Owner"
+    },
+    {
+        "firstName": "Mark",
+        "lastName": "Twain",
+        "type": "Consumer"
+    },
+    {
+        "firstName": "Justin",
+        "lastName": "Clark",
+        "type": "Business Owner"
+    }
+  ]
+  export default data
